0eedf5569b247d48ddd31f28b32d814c39dbc34d
parrn1 <mrnickp@comcast.net>
2019-08-30 09:42:53 -0400

    Add project files.

---
+++ inheritanceOveride/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritanceOveride
+{
+
+    public class CityBaseClass
+    {
+
+        public virtual void SayHello()
+        {
+            Console.Write("Hello World");
+        }
+
+        
+    }
+
+    public class CityDerivedClass : CityBaseClass
+    {
+        public override void SayHello()
+        {
+            Console.Write("Hello Everybody!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cityBaseInstance = new CityBaseClass();
+            cityBaseInstance.SayHello();
+
+            var cityOverideInstance = new CityDerivedClass();
+            cityOverideInstance.SayHello();
+
+            Console.ReadLine();
+        }
+    }
+}
